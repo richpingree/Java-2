e2711c59cc777f04590a43ee85c8854dd4c17526
richpingree <richpingree@fullsail.edu>
2015-01-07 21:24:56 -0600

    added xml and class files
    land and display frag xml, and item list frag class docs

---
+++ Java2FragmentsandFiles/app/src/main/java/com/richardpingree/java2fragmentsandfiles/ItemListFragment.java
@@ -0,0 +1,10 @@
+package com.richardpingree.java2fragmentsandfiles;
+
+import android.app.ListFragment;
+
+/**
+ * Created by richardpingree on 1/7/15.
+ */
+public class ItemListFragment extends ListFragment {
+
+}
---
+++ Java2FragmentsandFiles/app/src/main/java/com/richardpingree/java2fragmentsandfiles/MainActivity.java
@@ -0,0 +1,20 @@
+// Created By Richard Pingree
+
+package com.richardpingree.java2fragmentsandfiles;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
