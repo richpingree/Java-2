10adb80bc5397896c8723df3bdf7044b4f470059
richpingree <richpingree@fullsail.edu>
2015-01-08 18:48:52 -0600

    added network and asynctask
    added network and asynctask

--- Java2FragmentsandFiles/app/src/main/java/com/richardpingree/java2fragmentsandfiles/ItemListFragment.java
+++ Java2FragmentsandFiles/app/src/main/java/com/richardpingree/java2fragmentsandfiles/ItemListFragment.java
@@ -1,3 +1,5 @@
+//created by Richard Pingree
+
 package com.richardpingree.java2fragmentsandfiles;
 
 import android.app.ListFragment;
@@ -6,5 +8,7 @@ import android.app.ListFragment;
  * Created by richardpingree on 1/7/15.
  */
 public class ItemListFragment extends ListFragment {
+
+    public static final String TAG = "ItemListFragment.TAG";
 
 }
--- Java2FragmentsandFiles/app/src/main/java/com/richardpingree/java2fragmentsandfiles/MainActivity.java
+++ Java2FragmentsandFiles/app/src/main/java/com/richardpingree/java2fragmentsandfiles/MainActivity.java
@@ -2,19 +2,146 @@
 
 package com.richardpingree.java2fragmentsandfiles;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 
+public class MainActivity extends Activity implements View.OnClickListener {
 
-public class MainActivity extends ActionBarActivity {
+    EditText userInput;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.userText);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(this);
     }
 
+    protected boolean isOnline() {
+        ConnectivityManager connMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connMan.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else
+            return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        String getInput = userInput.getText().toString();
+
+        //replaces spaces with underscore and puts lowercase
+        getInput = getInput.replaceAll(" ", "_").toLowerCase();
+
+        if (isOnline()){
+            //Log.i("Test", "you have an internet connection.");
+            String baseURL = "http://api.artistlink.com/home/accounts.json?auth_token=5xVzCSGTz4yNaaxyJbcs";
+            URL queryURl = null;
+            try {
+                queryURl = new URL(baseURL + "&name=" + getInput);
+            } catch (MalformedURLException e) {
+                //e.printStackTrace();
+            }
+            //executes asynctask
+            new myTask().execute(queryURl);
+            userInput.setText("");
+        } else
+            //message for no network connection
+            Toast.makeText(getBaseContext(), "Not Connected to Network!", Toast.LENGTH_LONG).show();
+        //Log.i("Test", "internet not available");
+
+    }
+
+    private class myTask extends AsyncTask<URL, Integer, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(URL... urls) {
+
+            String jsonString = "";
+
+            //collect api response
+            for (URL queryURL: urls) {
+                try {
+                    URLConnection conn = queryURL.openConnection();
+                    jsonString = IOUtils.toString(conn.getInputStream());
+                    //removes brackets from api data
+                    jsonString = jsonString.replace("[","");
+                    jsonString = jsonString.replace("]","");
+                    break;
+                } catch (Exception e) {
+
+                    //Log.e("Test", "could not connect to network" + queryURL.toString());
+                }
+            }
+           Log.i("Test", "received data" + jsonString);
 
 
+            //api string to json
+
+            JSONObject apiData;
+
+            try {
+                apiData = new JSONObject(jsonString);
+            } catch (Exception e){
+
+                Log.e("Test", "can not convert");
+                apiData = null;
+            }
+
+            return apiData;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject apiData) {
+            //returns data to display
+            if (apiData != null){
+               // Artist result = new Artist(apiData);
+                //updateDisplay(result);
+
+
+            }else{
+                //alert to tell user that artist not in database
+
+                AlertDialog.Builder alertView = new AlertDialog.Builder(MainActivity.this);
+                alertView.setTitle("Artist Not Found!");
+                alertView.setMessage("Please try a different artist.");
+                alertView.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                alertView.show();
+            }
+
+
+
+        }
+    }
 }
+
+
+
