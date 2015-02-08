da81d8cf29b999b1884e4f710317f919e32b8137
richpingree <richpingree@fullsail.edu>
2015-02-08 11:36:31 -0600

    created apk and moved the menu options down
    created apk and moved the menu options down in the code

--- Fragment&File/app/src/main/java/com/richardpingree/fragmentfile/MainActivity.java
+++ Fragment&File/app/src/main/java/com/richardpingree/fragmentfile/MainActivity.java
@@ -51,28 +51,6 @@ public class MainActivity extends ActionBarActivity implements ArtistListFragmen
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     @Override
     public void displayArtist(String name, String genre, String label, String city, String state) {
@@ -101,4 +79,27 @@ public class MainActivity extends ActionBarActivity implements ArtistListFragmen
             frag.displayList();
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
