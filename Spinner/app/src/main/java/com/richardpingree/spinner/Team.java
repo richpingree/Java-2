6d3cd0744a4a202b2d899efc5435742d03e9a9cb
richpingree <richpingree@fullsail.edu>
2015-02-21 12:12:19 -0600

    added Team Class
    added Team Class and setup for 9 players

---
+++ Spinner/app/src/main/java/com/richardpingree/spinner/Team.java
@@ -0,0 +1,94 @@
+package com.richardpingree.spinner;
+
+/**
+ * Created by Richard Pingree on 2/21/15.
+ */
+public class Team {
+
+    public String mPlayer1;
+    public String mPlayer2;
+    public String mPlayer3;
+    public String mPlayer4;
+    public String mPlayer5;
+    public String mPlayer6;
+    public String mPlayer7;
+    public String mPlayer8;
+    public String mPlayer9;
+
+
+    public Team(){
+
+    }
+
+    public String getmPlayer1(){
+        return mPlayer1;
+    }
+
+    public void setmPlayer1(String player1){
+        mPlayer1 = player1;
+    }
+
+    public String getmPlayer2(){
+        return mPlayer2;
+    }
+
+    public void setmPlayer2(String player2){
+        mPlayer2 = player2;
+    }
+
+    public String getmPlayer3(){
+        return mPlayer3;
+    }
+
+    public void setmPlayer3(String player3){
+        mPlayer3 = player3;
+    }
+
+    public String getmPlayer4(){
+        return mPlayer4;
+    }
+
+    public void setmPlayer4(String player4){
+        mPlayer4 = player4;
+    }
+
+    public String getmPlayer5(){
+        return mPlayer5;
+    }
+
+    public void setmPlayer5(String player5){
+        mPlayer5 = player5;
+    }
+
+    public String getmPlayer6(){
+        return mPlayer6;
+    }
+
+    public void setmPlayer6(String player6){
+        mPlayer6 = player6;
+    }
+
+    public String getmPlayer7(){
+        return mPlayer7;
+    }
+
+    public void setmPlayer7(String player7){
+        mPlayer7 = player7;
+    }
+
+    public String getmPlayer8(){
+        return mPlayer8;
+    }
+
+    public void setmPlayer8(String player8){
+        mPlayer8 = player8;
+    }
+
+    public String getmPlayer9(){
+        return mPlayer9;
+    }
+
+    public void setmPlayer9(String player9){
+        mPlayer9 = player9;
+    }
+}
